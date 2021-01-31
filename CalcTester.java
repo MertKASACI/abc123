cc35d32e03378041be9003bb746273cb3c040ac9
Görkem Canıdemir <40427109+gcanidemir@users.noreply.github.com>
2021-01-31 18:19:22 +0300

    Add files via upload

---
+++ CalcTester.java
@@ -0,0 +1,106 @@
+//-----------------------------------------------------
+// Title: CalcTester
+// Author: Ömer Görkem Canıdemir
+// ID: 13177246034
+// Section: 2
+// Assignment: 1 Part_2
+// Precondition: Needs input.txt to run. Because it gets all the input data from it.
+// Description: This class is the tester for Calc.
+//-------------------------------------------------
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//Calc class extends the Stack class to use Node constructor.
+public class CalcTester extends Stack<Integer> {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner keyboard = new Scanner(new File("input.txt"));
+		Stack<Integer> operandDepot = new Stack<Integer>();
+		String input;
+		// It check the first part of the input and do the latter according to that.
+		do {
+			input = keyboard.nextLine();
+			String[] splittedInput = input.split(" ");
+			switch (splittedInput[0]) {
+			// It prints out all the items in the stack.
+			case "S":
+				try {
+					Node temp = operandDepot.getFirst();
+					System.out.print("[" + temp.getItem());
+					for (int i = 0; i < operandDepot.getSize() - 1; i++) {
+						temp = temp.getNext();
+						System.out.print(", " + temp.getItem());
+					}
+					System.out.println("]");
+				} catch (Exception e) {
+					System.out.println("[]");
+				}
+				break;
+			// It prints out and pops the last item in the stack.
+			case "P":
+				try {
+					int temp = operandDepot.pop();
+					System.out.println(temp);
+				} catch (Exception e) {
+					System.out.println("Stack is empty. Please add operands.");
+				}
+				break;
+			// It adds the last two item in the stack.
+			case "+":
+				try {
+					int operand1 = operandDepot.pop();
+					int operand2 = operandDepot.pop();
+					operandDepot.push(operand2 + operand1);
+				} catch (Exception e) {
+					System.out.println("There is not enough number in the depot. Please first enter the numbers.");
+				}
+				break;
+			// It subtracts the last two item in the stack.
+			case "-":
+				try {
+					int operand1 = operandDepot.pop();
+					int operand2 = operandDepot.pop();
+					operandDepot.push(operand2 - operand1);
+				} catch (Exception e) {
+					System.out.println("There is not enough number in the depot. Please first enter the numbers.");
+				}
+				break;
+			// It multiplies the last two item in the stack.
+			case "*":
+				try {
+					int operand1 = operandDepot.pop();
+					int operand2 = operandDepot.pop();
+					operandDepot.push(operand2 * operand1);
+				} catch (Exception e) {
+					System.out.println("There is not enough number in the depot. Please first enter the numbers.");
+				}
+				break;
+			// It divides the last two item in the stack.
+			case "/":
+				try {
+					int operand1 = operandDepot.pop();
+					int operand2 = operandDepot.pop();
+					operandDepot.push(operand2 / operand1);
+				} catch (Exception e) {
+					System.out.println("There is not enough number in the depot or Tried to divide 0.");
+				}
+				break;
+			// It gets the written input and pushes into the stack.
+			default:
+				if (!splittedInput[0].equalsIgnoreCase("E")) {
+					try {
+						for (String s : splittedInput) {
+							int inputInt = Integer.parseInt(s);
+							operandDepot.push(inputInt);
+						}
+					} catch (Exception e) {
+						System.out.println("Please Enter the Operation or a operand.");
+					}
+				}
+			}
+		} while (!input.equalsIgnoreCase("E"));
+		keyboard.close();
+	}
+}
---
+++ Stack.java
@@ -0,0 +1,55 @@
+//-----------------------------------------------------
+// Title: Stack
+// Author: Ömer Görkem Canıdemir
+// ID: 13177246034
+// Section: 2
+// Assignment: 1 Part_2
+// Description: This class defines a Stack object.
+//-------------------------------------------------
+
+public class Stack<Item> {
+	private Node first = null;
+	private int size = 0;
+
+	protected class Node {
+		Item item;
+		Node next;
+
+		public Item getItem() {
+			return item;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+	}
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+
+	public void push(Item item) {
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldfirst;
+		size++;
+	}
+
+	public Item pop() {
+		Item item = first.item;
+		first = first.next;
+		size--;
+		return item;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public Node getFirst() {
+		return first;
+	}
+
+}
